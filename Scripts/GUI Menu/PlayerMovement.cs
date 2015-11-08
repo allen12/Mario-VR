30a1fc8d44a20d423c5542a556e668e7ee64670a
allen12 <computer_allen@yahoo.com>
2015-11-08 13:16:16 -0500

    GUI Initial Commit. Highlights buttons when camera moves towards them.

---
+++ Scripts/GUI Menu/PlayerMovement.cs
@@ -0,0 +1,93 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed;
+    public float jumpHeight;
+    public bool freezeRotation;
+    public Transform groundCheck;
+    public float groundCheckRadius;
+    public LayerMask whatIsGround;
+
+    private bool grounded;
+
+    private bool doubleJump;
+
+    float lockPos = 0;
+
+    // Use this for initialization
+    void Start()
+    {
+        doubleJump = false;
+    }
+
+    void FixedUpdate()
+    {
+        //Debug.Log (grounded);
+
+        if (grounded)
+        {
+            doubleJump = false;
+            //string s = "Grounded";
+            //Debug.Log(s);
+        }
+        if (Input.GetKeyDown(KeyCode.Space) && grounded)
+        {
+            string s = "Getting in here";
+            Debug.Log(s);
+            Jump();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space) && Input.GetKeyDown(KeyCode.P) && !doubleJump && !grounded)
+        {
+            string s = "Not Grounded";
+            Debug.Log(s);
+            Jump();
+            doubleJump = true;
+        }
+
+        //directions
+        if (Input.GetKey(KeyCode.L))
+        {
+            GetComponent<Rigidbody>().AddForce(transform.right * moveSpeed, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.J))
+        {
+            GetComponent<Rigidbody>().AddForce(-transform.right * moveSpeed, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.I))
+        {
+            GetComponent<Rigidbody>().AddForce(transform.forward * moveSpeed, ForceMode.Acceleration);
+        }
+        if (Input.GetKey(KeyCode.K))
+        {
+            GetComponent<Rigidbody>().AddForce(-transform.forward * moveSpeed, ForceMode.Acceleration);
+        }
+
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "Ground")
+        {
+            grounded = true;
+        }
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.rotation = Quaternion.Euler(lockPos, lockPos, lockPos);
+    }
+
+    public void Jump()
+    {
+        GetComponent<Rigidbody>().velocity = new Vector3(GetComponent<Rigidbody>().velocity.x, jumpHeight);
+
+        grounded = false;
+    }
+
+
+}
---
+++ Scripts/GUI Menu/PointerEventsController.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+using System.Collections;
+using System;
+
+public class PointerEventsController : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
+{
+    public void OnPointerEnter(PointerEventData eventData)
+    {
+        if (this.gameObject.name == "Start Game Button")
+        {
+            // this.gameObject.GetComponent...
+            // ...OR...
+
+            GameObject.Find("Start Game Button").GetComponent<Button>().onClick.RemoveAllListeners();
+            GameObject.Find("Start Game Button").GetComponent<Button>().onClick.AddListener(TransitionLevel);
+        }
+
+        if (this.gameObject.name == "Quit Button")
+        {
+            this.gameObject.GetComponent<Button>().onClick.RemoveAllListeners();
+            this.gameObject.GetComponent<Button>().onClick.AddListener(QuitGame);
+        }
+    }
+
+    private void QuitGame()
+    {
+        Application.Quit();
+    }
+
+    public void OnPointerExit(PointerEventData eventData)
+    {
+        throw new NotImplementedException();
+    }
+
+    void TransitionLevel()
+    {
+        Application.LoadLevel(1);
+    }
+
+
+    // Use this for initialization
+    void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
